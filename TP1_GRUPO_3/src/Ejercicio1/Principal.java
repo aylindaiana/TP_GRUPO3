387b519911c05a75ac80b927e817c3166783cea1
AntonioMelino <antoniomelino1997@gmail.com>
2025-03-27 11:17:45 -0300

    Resolución del punto 8.

--- TP1_GRUPO_3/src/Ejercicio1/Principal.java
+++ TP1_GRUPO_3/src/Ejercicio1/Principal.java
@@ -3,24 +3,31 @@ package Ejercicio1;
 public class Principal {
 	public static void main(String[] args)
 	{
-		Empleado x = new Empleado();
-		x.setNombre("Jose");
-		x.setEdad(30);
-		System.out.println(x.toString());
-		
-		Empleado p = new Empleado();
-		p.setNombre("Pepe");
-		p.setEdad(30);
-		System.out.println(p.toString());
-		
-		Empleado a = new Empleado();
-		a.setNombre("Ariel");
-		a.setEdad(30);
-		System.out.println(a.toString());
-		
-		Empleado b = new Empleado("Pedro", 25);
-		System.out.println(b.toString());
-		
-		System.out.println("El próximo ID será el " + Empleado.devuelveProximoID());
+		// Crear 5 empleados (algunos con constructor vacío y otros con parámetros)
+        Empleado emp1 = new Empleado(); // Constructor vacío
+        emp1.setNombre("Marta");
+        emp1.setEdad(28);
+        
+        Empleado emp2 = new Empleado("Carlos", 35); // Constructor con parámetros
+        
+        Empleado emp3 = new Empleado(); // Constructor vacío
+        emp3.setNombre("Laura");
+        emp3.setEdad(32);
+        
+        Empleado emp4 = new Empleado("Juan", 40); // Constructor con parámetros
+        
+        Empleado emp5 = new Empleado(); // Constructor vacío
+        emp5.setNombre("Ana");
+        emp5.setEdad(27);
+
+        // Mostrar información de los empleados usando el método toString()
+        System.out.println(emp1.toString());
+        System.out.println(emp2.toString());
+        System.out.println(emp3.toString());
+        System.out.println(emp4.toString());
+        System.out.println(emp5.toString());
+
+        // Mostrar la información que devuelve el método devuelveProximoId()
+        System.out.println("\nEl próximo ID disponible será: " + Empleado.devuelveProximoID());
 	}
 }
