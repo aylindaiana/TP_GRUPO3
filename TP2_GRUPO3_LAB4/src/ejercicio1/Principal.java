9a19197043d8963ee588e981a440f1d44a6733d8
AntonioMelino <antoniomelino1997@gmail.com>
2025-04-01 13:03:12 -0300

    Resolución puntos 3 y 4

--- TP2_GRUPO3_LAB4/src/ejercicio1/Principal.java
+++ TP2_GRUPO3_LAB4/src/ejercicio1/Principal.java
@@ -3,8 +3,4 @@ package ejercicio1;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
--- TP2_GRUPO3_LAB4/src/ejercicio1/Profesor.java
+++ TP2_GRUPO3_LAB4/src/ejercicio1/Profesor.java
@@ -6,3 +6,43 @@ public class Profesor extends Empleado {
 	private int antiguedadDocente;
 	
 }
+
+//Constructores
+public Profesor() {
+	super();
+	this.cargo= "sin cargo";
+	this.antiguedadDocente= 0;
+}
+
+public Profesor(string nombre, int edad, string cargo, int antiguedadDocente) {
+	super(nombre, edad);
+	this.cargo= cargo;
+	this.antiguedadDocente= antiguedadDocente;
+}
+
+//Getters y Setters
+public String getCargo() {
+    return cargo;
+}
+
+public void setCargo(String cargo) {
+    this.cargo = cargo;
+}
+
+public int getAntiguedadDocente() {
+    return antiguedadDocente;
+}
+
+public void setAntiguedadDocente(int antiguedadDocente) {
+    this.antiguedadDocente = antiguedadDocente;
+}
+
+//Método toString() sobreescrito
+@Override
+public String toString() {
+    return "Profesor [ID: " + getId() + 
+           ", Nombre: " + getNombre() + 
+           ", Edad: " + getEdad() + 
+           ", Cargo: " + cargo + 
+           ", Antigüedad docente: " + antiguedadDocente + " años]";
+}
