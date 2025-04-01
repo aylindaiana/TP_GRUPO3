722580438ad2c551217fe46582777a8c33ae321f
Alexander <alexcarvajalcontacto@gmail.com>
2025-04-01 16:35:49 -0300

    Fixes

--- TP2_GRUPO3_LAB4/src/ejercicio1/Principal.java
+++ TP2_GRUPO3_LAB4/src/ejercicio1/Principal.java
@@ -3,4 +3,7 @@ package ejercicio1;
 public class Principal {
 
 	public static void main(String[] args) {
+		Profesor a = new Profesor("Profesor Jackson", 30, "Profesor de Matematicas", 5);
+		System.out.println(a.toString());
+	}
 }
