d54c41c538fc6451e7484f29ddf9f3ea6723ce9f
rodriguezleandro98 <63465148+rodriguezleandro98@users.noreply.github.com>
2025-04-15 11:14:14 -0300

    puntob

--- TP3_GRUPO3_LAB4/src/ejercicio1/Main.java
+++ TP3_GRUPO3_LAB4/src/ejercicio1/Main.java
@@ -9,14 +9,31 @@ import java.util.TreeSet;
 public class Main {
 
 	public static void main(String[] args) {
-		try (BufferedReader br = new BufferedReader(new FileReader("Personas.txt"))) {
+		Set<Persona> personas = new TreeSet<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("Personas.txt"))) {
             String linea;
             while ((linea = br.readLine()) != null) {
-                System.out.println("Línea leída: " + linea);
+            	//System.out.println("Línea leída: " + linea);
+                String[] partes = linea.split("-");
+                
+                if (partes.length == 3) {
+                    String nombre = partes[0].trim();
+                    String apellido = partes[1].trim();
+                    String dni = partes[2].trim();
+                    
+                    try {
+                        DniVerificador.verificarDniInvalido(dni);
+                        personas.add(new Persona(nombre, apellido, dni));
+                    } catch (DniInvalido e) {
+                        System.out.println("DNI inválido para " + nombre + " " + apellido + ": " + e.getMessage());
+                    }
+                } else {
+                    System.out.println("Línea con formato incorrecto: " + linea);
+                }
             }
         } catch (IOException e) {
             System.out.println("Error al leer el archivo: " + e.getMessage());
         }
 	}
-
 }
