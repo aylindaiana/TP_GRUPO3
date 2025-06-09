46534409611f286e374d9c5b79e2646d8f89ccdc
BraianPirelli <Braianpirelli@gmail.com>
2025-06-09 20:10:14 -0300

    Inicio TP-Integrador
    creación de estructura general de arquitectura en capas y clase conexión.

---
+++ TPINT_GRUPO_3_LAB4/src/main/java/daoImpl/Conexion.java
@@ -0,0 +1,41 @@
+package daoImpl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public static Conexion instancia;
+    private Connection connection;
+
+    private Conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            //Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/-----> AGREGAR EL NOMBRE DE LA FUTURA BBDD <-----", "root", "root");
+            this.connection.setAutoCommit(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Conexion getConexion() {
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+
+    public Connection getSQLConexion() {
+        return this.connection;
+    }
+
+    public void cerrarConexion() {
+        try {
+            this.connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        instancia = null;
+    }
+}
